0e5c0a30f3382aa9a4b0a2d9d4bd9ece442b60af
jimmy <jimishukurow@gmail.com>
2025-12-14 21:22:28 +0500

    initial commit

---
+++ src/main/java/info/jemsit/SimSimGateway/SimSimGatewayApplication.java
@@ -0,0 +1,13 @@
+package info.jemsit.SimSimGateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimSimGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimSimGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/info/jemsit/SimSimGateway/SimSimGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package info.jemsit.SimSimGateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimSimGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
