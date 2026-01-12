01b8a0d9dd8db60304ce464be7fa8746c40a6443
jimmy <jimishukurow@gmail.com>
2026-01-12 16:19:54 +0500

    refactor: migrate application properties to YAML format and add CORS configuration

--- src/main/java/info/jemsit/SimSimGateway/SimSimGatewayApplication.java
+++ src/main/java/info/jemsit/SimSimGateway/SimSimGatewayApplication.java
@@ -2,8 +2,10 @@ package info.jemsit.SimSimGateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SimSimGatewayApplication {
 
 	public static void main(String[] args) {
